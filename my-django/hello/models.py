a2749713ac08958e89ca166ea3ada2e920023c34
JangWonJong <jwj9922@naver.com>
2022-03-02 17:59:00 +0900

    수업종료

--- my-django/hello/models.py
+++ my-django/hello/models.py
@@ -1,7 +1,7 @@
-
+import random
 def main():
     while 1:
-        menu = input('0.Exit 1. 계산기 (+, - ,* ,/) 2.BMI 3.성적표')
+        menu = input('0.Exit 1. 계산기 (+, - ,* ,/) 2.BMI 3.성적표 4.? 5.주사위 6.Q5 7.Q6')
         if menu == 0:
             break
         elif menu == '1':  # 계산기
@@ -14,7 +14,7 @@ def main():
 
         elif menu == '3':  # 성적표
             for i in ['김유신', '강감찬', '유관순', '장원종', '스칼라']:
-                print()
+                print(i)
             q3 = Quiz03Grade((input('이름')), int(input('언어점수')), int(input('영어점수')), int(input('수학점수')))
             print(f'\n#### 성적표 ####\n+ * * 이름: {q3.name}\n'
                   f' * * 언어점수: {q3.kor}\n'
@@ -25,15 +25,17 @@ def main():
                   f' * * 합격여부: {q3.grade()} \n')
 
         elif menu == '5':
-            q5 = Quiz05Dice()
+            print(Quiz05Dice.cast())
 
         elif menu == '6':
-            q6 = Quiz06RandomGenerator()
-
+            q6 = None
         elif menu == '7':
             q7 = Quiz07RandomChoice()
+            print(f'random : {q7.chooseMember()}')
 
-
+        elif menu =='8':
+            q8 = Quiz08Rps(1) # 가위 1 바위 2 보 3
+            print(q8.game())
 
 class Quiz01Calculator(object):
 
@@ -42,24 +44,15 @@ class Quiz01Calculator(object):
         self.num2 = num2
         self.opcode = opcode
 
-    def add(self):
-        return self.num1 + self.num2
-    def sub(self):
-        return self.num1 - self.num2
-    def mul(self):
-        return self.num1 * self.num2
-    def div(self):
-        return self.num1 / self.num2
-
     def calc(self):
         if self.opcode == '+':
-            return self.add()
+            return self.num1 + self.num2
         elif self.opcode == '-':
-            return self.sub()
+            return self.num1 - self.num2
         elif self.opcode == '*':
-            return self.mul()
+            return self.num1 * self.num2
         elif self.opcode == '/':
-            return self.div()
+            return self.num1 / self.num2
 
 class Quiz02Bmi(object):
     def __init__(self, name, height, weight):
@@ -123,23 +116,86 @@ class Quiz04GradeAuto(object):
     def chkPass(self):# 60점이상이면 합격
         pass
 
+def myRandom(start, end):
+    return random.randint(start, end)
+
 class Quiz05Dice(object):
-    def __init__(self):
-        pass
+    @staticmethod
+    def cast():
+        return myRandom(1,6)
 
-class Quiz06RandomGenerator(object):
-    def __init__(self): # 원하는 범위의 정수에서 랜덤값 1개 추출
 
-        pass
 class Quiz07RandomChoice(object):
     def __init__(self): # 803호에서 랜덤으로 1명 이름 추출
         self.members = ['홍정명', '노홍주', '전종현', '정경준', '양정오',
                         "권혜민", "서성민", "조현국", "김한슬", "김진영",
-                        '심민혜' , '권솔이', '김지혜' , '하진희' , '최은아',
+                        '심민혜', '권솔이', '김지혜', '하진희', '최은아',
                         '최민서', '한성수', '김윤섭', '김승현',
-                        "강 민", "최건일", "유재혁", "김아름", "장원종" ]
+                        "강 민", "최건일", "유재혁", "김아름", "장원종"]
+    def chooseMember(self):
+        ran = myRandom(0,23)
+        return self.members[ran]
+
+class Quiz08Rps(object):
+    def __init__(self, user):
+        self.user = user
+        self.com = myRandom(1,3)
+
+    def game(self):
+        c = self.com
+        u = self.user
+        # 1 가위 2 바위 3 보
+        rps = ['가위', '바위', '보']
+        if u == 1:
+            if c == 1:
+                res = f'플레이어:{rps[0]}, 컴퓨터{rps[0]}, 결과 무승부'
+            elif c == 2:
+                res = f'플레이어:{rps[0]}, 컴퓨터{rps[1]}, 결과 패배'
+            elif c == 3:
+                res = f'플레이어:{rps[0]}, 컴퓨터{rps[2]}, 결과 승리'
+        if u == 2:
+            if c == 1:
+                res = f'플레이어:{rps[1]}, 컴퓨터{rps[0]}, 결과 승리'
+            elif c == 2:
+                res = f'플레이어:{rps[1]}, 컴퓨터{rps[1]}, 결과 무승부'
+            elif c == 3:
+                res = f'플레이어:{rps[1]}, 컴퓨터{rps[2]}, 결과 패배'
+        if u == 3:
+            if c == 1:
+                res = f'플레이어:{rps[2]}, 컴퓨터{rps[0]}, 결과 패배'
+            elif c == 2:
+                res = f'플레이어:{rps[2]}, 컴퓨터{rps[1]}, 결과 승리'
+            elif c == 3:
+                res = f'플레이어:{rps[2]}, 컴퓨터{rps[2]}, 결과 무승부'
+        else:
+                res = '1~3입력'
+
+        return res
 
 
+class Quiz09GetPrime(object):
+    def __init__(self):
+        pass
+
+class Quiz10LeapYear(object):
+    def __init__(self):
+        pass
+
+class Quiz11NumberGolf(object):
+    def __init__(self):
+        pass
+
+class Quiz12Lotto(object):
+    def __init__(self):
+        pass
+
+class Quiz13Bank(object): # 이름, 입금, 출금만 구현
+    def __init__(self):
+        pass
+
+class Quiz14Gugudan(object): # 책받침구구단
+    def __init__(self):
+        pass
 
 if __name__ == '__main__':
     main()
