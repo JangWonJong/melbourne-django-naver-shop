77ee0eff12d0a0d664c42508b007e128778d7524
JangWonJong <jwj9922@naver.com>
2022-03-04 14:14:59 +0900

    2022-03-04

---
+++ my-django/hello/quiz30.py
@@ -0,0 +1,44 @@
+class Quiz30:
+    def __init__(self):
+        pass
+
+    def quiz30list(self):
+        list1 = [1,2,3,4]
+        #print(list1,type(list1))
+        list2 = ['math', 'english']
+        print(list1[0], list1[-1], list1[-2], list1[1:3])
+        print(list2[0])
+        print(list[0][1])
+        list3 = [1, '2', [1, 2, 3]]
+        list4 = [1,2,3]
+        print(2*list4)
+        list5 = [4,5]
+        print(list4+list5)
+        print(list4.append(list5))
+
+        list6 = [[1,2],[0,5]]
+        # 2를 출력해보시오
+        a = [1,2]
+        b = [0,5]
+        c = [a,b]
+        print(list6[0][1])
+
+
+
+
+
+
+
+
+
+
+
+    def quiz31tuple(self):
+        tuple1 = (1,2)
+        tuple2 = (0,(1,4))
+        print(tuple1+tuple2)
+
+
+
+    def quiz32(self):
+        pass
